17cef0d0e74e6e1a09c467a66f4bd7a995856f20
Novotny, Jiri <jnovotny@netsuite.com>
2020-09-09 13:59:43 +0200

    Implementing with result "fizz"

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,4 +1,7 @@
 function fizzbuzz(a) {
+  if (a % 3 == 0) {
+    return "fizz";
+  }
   return a;
 }
 module.exports = fizzbuzz;
--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -3,3 +3,7 @@ const fizzbuzz = require("./fizzbuzz");
 test("fizzbuzz of 1 is 1", () => {
   expect(fizzbuzz(1)).toBe(1);
 });
+
+test("fizzbuzz of 3 is fizz", () => {
+  expect(fizzbuzz(3)).toBe("fizz");
+});

51f7a0d65c1fff57793abb448a3dd42fa64df818
Novotny, Jiri <jnovotny@netsuite.com>
2020-09-09 14:08:32 +0200

    Extracting conditions into separate functions

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,14 +1,23 @@
 function fizzbuzz(a) {
-  if (a % 3 == 0 && a % 5 == 0) {
+  if (isFizz(a) && isBuzz(a)) {
     return "fizzbuzz";
   }
-  if (a % 3 == 0) {
+  if (isFizz(a)) {
     return "fizz";
   }
-  if (a % 5 == 0) {
+  if (isBuzz(a)) {
     return "buzz";
   }
 
   return a;
 }
+
+function isFizz(a) {
+  return a % 3 == 0;
+}
+
+function isBuzz(a) {
+  return a % 5 == 0;
+}
+
 module.exports = fizzbuzz;
